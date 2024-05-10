b3072f6488cf5bf207f77e61a63fb6967ceb6a1a
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-05-10 13:29:24 +0530

    implement auth, appwrite, and sentry

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 });
 
 export const metadata: Metadata = {
-  title: "iBanking",
+  title: "iBanking - The Modern Banking Platform",
   description: "A modern banking platform for everyone.",
   icons: {
     icon: "/icons/logo.svg",
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -5,6 +5,7 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -69,6 +70,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 })}
               </nav>
             </SheetClose>
+            <Footer user={user} type="mobile" />
           </div>
         </SheetContent>
       </Sheet>
